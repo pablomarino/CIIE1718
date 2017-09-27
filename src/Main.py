2fbf6b5d1382aca28e13600eea4bc4debd6a8dad
Pablo Mariño Boga <pablo.marino.boga@gmail.com>
2017-09-27 13:38:23 +0200

    First commit

--- src/Main.py
+++ src/Main.py
@@ -1,33 +1,40 @@
 import pygame
 import sys
-from src.data.DataRetriever import loadPreferences, printPreferences
+from src.data.DataRetriever import DataRetriever
 
 clock = pygame.time.Clock()
-screenFlags = pygame.DOUBLEBUF | pygame.HWSURFACE;
+screenFlags = pygame.DOUBLEBUF | pygame.HWSURFACE
 finished = False
 
+# Cargo preferencias de la aplicacion
+data = DataRetriever()
+data.loadPreferences('../bin/config/preferences.json')
+fps = data.getFps();
 
-# Preferencias
-preferences = loadPreferences('../bin/config/preferences.json')
-printPreferences(preferences)
-screenSize = w, h = int(preferences['screen_res'][0]), int(preferences['screen_res'][1])
-screenIcon = preferences['screen_icon']
-screenTitle = preferences['screen_title']
-fps = int(preferences['fps_target'])
+# Cargo datos de niveles
+data.loadLevels('../bin/config/levels.json')
+
+# Cargo datos de los jugadores
+data.loadPlayers('../bin/config/players.json')
+#print(data.getPlayerSheet('beatrice'))
+
 
 # Inicializar la libreria de pygame
 pygame.init()
 
 # Creamos la pantalla
-pygame.display.set_caption(screenTitle)
-pygame.display.set_icon(pygame.image.load(screenIcon))
-screen = pygame.display.set_mode(screenSize, screenFlags)
+pygame.display.set_caption(data.getWindowTitle())
+pygame.display.set_icon(pygame.image.load(data.getWindowIcon()))
+screen = pygame.display.set_mode([data.getWidth(), data.getHeight()], screenFlags)
 
 while not finished:
-    now = clock.tick(fps)
+    clock.tick(fps)
     for e in pygame.event.get():
-        if e.type == pygame.KEYDOWN and e.key == int(preferences['keys']['quit']):
+        if e.type == pygame.KEYDOWN and e.key == int(data.getKeyQuit()):
             finished = True
-#
+
+# Aqui ocurre la magia
+
     pygame.display.flip()
 sys.exit()
+
--- src/data/DataRetriever.py
+++ src/data/DataRetriever.py
@@ -1,23 +1,111 @@
 import json
 
-#@staticmethod
-def loadPreferences(file):
-    with open(file) as preferences_file:
-        data = json.load(preferences_file)
-    return data
+class DataRetriever:
 
-#@staticmethod
-def printPreferences(data):
-    print('\nPreferences\n==================')
-    print('Version     : ' + data['version'])
-    print('Resolution  : ' + data['screen_res'][0] + 'x' + data['screen_res'][1])
-    print('Screen Icon : ' + data['screen_icon'])
-    print('Screen Title: ' + data['screen_title'])
-    print('Fps         : ' + data['fps_target'])
-    print('Keys quit   : ' + data['keys']['quit'][0] + ', ' + data['keys']['quit'][1])
-    print('Keys up     : ' + data['keys']['up'][0] + ', ' + data['keys']['up'][1])
-    print('Keys down   : ' + data['keys']['down'][0] + ', ' + data['keys']['down'][1])
-    print('Keys left   : ' + data['keys']['left'][0] + ', ' + data['keys']['left'][1])
-    print('Keys right  : ' + data['keys']['right'][0] + ', ' + data['keys']['right'][1])
-    print('Keys bt1    : ' + data['keys']['bt1'][0] + ', ' + data['keys']['bt1'][1])
-    print('Keys bt2    : ' + data['keys']['bt2'][0] + ', ' + data['keys']['bt2'][1])
+    # LOADERS
+    # TODO : ERRORES LOAD
+    def __init__(self):
+        self.__preferences = ''
+        self.__levels = ''
+        self.__players = ''
+
+    def __loadConfig(self, file):
+        with open(file) as preferences_file:
+            data = json.load(preferences_file)
+        return data
+
+    def loadPreferences(self,file):
+        self.__preferences = self.__loadConfig(file)
+
+    def loadLevels(self,file):
+        self.__levels = self.__loadConfig(file)
+
+    def loadPlayers(self,file):
+        self.__players = self.__loadConfig(file)
+
+
+    # GETTERS
+    # TODO : Errores GETTERS
+
+    def getPreferences(self):
+        return self.__preferences
+
+    def getVersion(self):
+        return self.__preferences['version']
+
+    def getWidth(self):
+        return int(self.__preferences['screen_res'][0])
+
+    def getHeight(self):
+        return int(self.__preferences['screen_res'][1])
+
+    def getWindowIcon(self):
+        return self.__preferences['window_icon']
+
+    def getWindowTitle(self):
+        return self.__preferences['window_title']
+
+    def getFps(self):
+        return float(self.__preferences['fps_target'])
+
+    def getKeyQuit(self):
+        return self.__preferences['keys']['quit']
+
+    def getKeyUp(self):
+        return self.__preferences['keys']['up']
+
+    def getKeyDown(self):
+        return self.__preferences['keys']['down']
+
+    def getKeyLeft(self):
+        return self.__preferences['keys']['left']
+
+    def getKeyRight(self):
+        return self.__preferences['keys']['right']
+
+    def getKeyBt1(self):
+        return self.__preferences['keys']['bt1']
+
+    def getKeyBt2(self):
+        return self.__preferences['keys']['bt2']
+
+    def getmedia_suffixes(self):
+        return self.__preferences['media_suffixes']
+
+
+    def getLevels(self):
+        return self.__levels
+
+    def getLevelSheet(self,id):
+        return self.__levels['world'][id]
+
+
+
+    def getPlayers(self):
+        return self.__players
+
+    def getPlayerSheet(self,id):
+        return self.__players["roster"][id]
+
+
+
+    # SETTERS
+
+
+    # UTILS
+
+
+    def printPreferences(self):
+        print('\nPreferences\n==================')
+        print('Version     : ' +  self.__preferences['version'])
+        print('Resolution  : ' +  self.__preferences['screen_res'][0] + 'x' +  self.__preferences['screen_res'][1])
+        print('Screen Icon : ' +  self.__preferences['screen_icon'])
+        print('Screen Title: ' +  self.__preferences['screen_title'])
+        print('Fps         : ' +  self.__preferences['fps_target'])
+        print('Keys quit   : ' +  self.__preferences['keys']['quit'][0] + ', ' +  self.__preferences['keys']['quit'][1])
+        print('Keys up     : ' +  self.__preferences['keys']['up'][0] + ', ' +  self.__preferences['keys']['up'][1])
+        print('Keys down   : ' +  self.__preferences['keys']['down'][0] + ', ' +  self.__preferences['keys']['down'][1])
+        print('Keys left   : ' +  self.__preferences['keys']['left'][0] + ', ' +  self.__preferences['keys']['left'][1])
+        print('Keys right  : ' +  self.__preferences['keys']['right'][0] + ', ' +  self.__preferences['keys']['right'][1])
+        print('Keys bt1    : ' +  self.__preferences['keys']['bt1'][0] + ', ' +  self.__preferences['keys']['bt1'][1])
+        print('Keys bt2    : ' +  self.__preferences['keys']['bt2'][0] + ', ' +  self.__preferences['keys']['bt2'][1])
