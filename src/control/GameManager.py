b9b8927711b1838e74d516038f2c54d99abb7b33
Matias <matias@Rocky.com>
2017-10-10 21:22:01 +0200

    Funciones para cambiar de escnea creadas

--- src/control/GameManager.py
+++ src/control/GameManager.py
@@ -8,7 +8,7 @@ class GameManager:
         self.data = data
         self.stack = []
         self.screenFlags = pygame.DOUBLEBUF | pygame.HWSURFACE
-        self.finished = False
+        self.finished_scene = False
         self.clock = None
         self.fps = None
         # Instancio el cargador de medios
@@ -27,19 +27,34 @@ class GameManager:
     def add(self, level):
         self.stack.append(level)
 
+    def changeScene(self):
+        self.finished_scene=True
+        if len(self.stack)>0:
+            self.stack.pop() 
+
+    def endGame(self):
+        self.stack=[]
+        self.finished=True
+
+    def addScene(self, scene):
+        self.finished_scene=True
+        self.stack.append(scene)
+
     def run(self):
         while not self.finished:
             time = self.clock.tick(self.fps)
             for e in pygame.event.get():
                 # Se sale al pulsar Esc
                 if e.type == pygame.KEYDOWN and e.key == int(self.data.getKeyQuit()):
-                    self.finished = True
+                    self.endGame()
                 # Call 'events' function in the current level
-                self.stack[0].events()
+                if len(self.stack)>0:
+                    self.stack[len(self.stack)-1 ].events()
 
             #   Aqui ocurre la magia (HardCoded)
-            self.stack[0].update(time)
-            self.stack[0].draw()
+            if len(self.stack)>0:
+                self.stack[len(self.stack)-1].update(time)
+                self.stack[len(self.stack)-1].draw()
 
             pygame.display.flip()
         pygame.quit()
