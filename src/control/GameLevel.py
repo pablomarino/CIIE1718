6a391d3386959ed1af05b85bc074720441b9f287
Jose Luis Álvarez Quiroga <joseluisalvquiroga@gmail.com>
2017-10-24 00:07:04 +0200

    Added a new TODO and enabled music again

--- src/control/GameLevel.py
+++ src/control/GameLevel.py
@@ -1,11 +1,8 @@
 # -*- encoding: utf-8 -*-
 
-import pygame
-
 from stage.Stage import *
+from view.Enemy import *
 from view.Player import Player
-from view.Enemy import Asmodeo
-import sys
 
 
 def str_to_class(str):
@@ -21,7 +18,7 @@ class GameLevel:
 
         # Add music to the game
         pygame.mixer.music.load(self.data.getMusicFile())
-        # pygame.mixer.music.play()
+        pygame.mixer.music.play()
 
         self.spriteGroup = pygame.sprite.Group()
         self.platformGroup = pygame.sprite.Group()
@@ -29,11 +26,10 @@ class GameLevel:
         self.enemyGroup = pygame.sprite.Group()
 
         if self.level:
+            # TODO solo crear nuevo jugador si es 'none', si no recuperar instancia anterior
             self.player = Player(manager, data, 'player')
             self.player.setPosition(data.getPlayerPositionAt(id))
             self.spriteGroup.add(self.player)
-
-            # TODO crear enemigos de una forma más natural
 
             for e in self.level["enemies"]:
                 tmp = str_to_class(e["kind"])(manager, data)
